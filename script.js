60c03e53777a412650a12d9c242f4696c201c0ad
ViktoriiaPatoka <viktoriia.patoka@nure.ua>
2026-03-25 16:47:47 +0200

    change main-code

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+let price = 500;
+let discount = 50;
+
+let finalPrice = price - discount;
+
+console.log("Початкова ціна: " + price + " грн");
+console.log("Знижка: " + discount + " грн");
+console.log("Ціна до сплати: " + finalPrice + " грн");
+
+if (finalPrice < 1000) {
+    console.log("Доставка буде платною.");
+} else {
+    console.log("Ви отримали безкоштовну доставку!");
+}
