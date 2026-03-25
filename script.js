64d0a7ef08817a218671d134e71eea68b4d23472
Yehor-Zadorozhnyi <yehor.zadorozhnyi@nure.ua>
2026-03-25 17:26:16 +0200

    Додано основний код программи

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+
 // Алгоритм сортування бульбашкою
 function bubbleSort(arr) {
     let n = arr.length;
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
-    }
+            if (arr[j] > arr[j + 1]) {
+                // Обмін елементів
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+    };
+    return arr;
+};
+
+// Приклад використання
+let array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Невідсортований масив: " + array);
+let sortedArray = bubbleSort(array);
+console.log("Відсортований масив: " + sortedArray); 
