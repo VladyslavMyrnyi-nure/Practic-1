fefb2d096692e23fe4d53ad1001b3d98a4701726
ViktoriiaPatoka <viktoriia.patoka@nure.ua>
2026-03-25 17:35:31 +0200

    add main-code js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+// Обчислення фінальної ціни з урахуванням знижки
 let price = 500;
-let discount = 50;
-
+let discount = 20;
 let finalPrice = price - discount;
 
 console.log("Початкова ціна: " + price + " грн");
 console.log("Знижка: " + discount + " грн");
-console.log("Ціна до сплати: " + finalPrice + " грн");
+console.log("Ціна до сплату: " + finalPrice + " грн");
 
 if (finalPrice < 1000) {
     console.log("Доставка буде платною.");
