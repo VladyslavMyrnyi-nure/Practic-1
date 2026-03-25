89bfb4cfcb5665280f1bce57ea0b47e0ff0ea72f
Artem Bilous <artem.bilous1@nure.ua>
2026-03-25 21:46:08 +0000

    Revert "Feature/bilous"

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-console.log("Hello, World!");
-
-function hello() {
-    console.log("Hello, World!");
-}
-hello();
-
-function factorial(n) {
-    if (n < 0) {
-        return null;
-    }
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
-console.log(factorial(5));
 // Обчислення фінальної ціни з урахуванням знижки
 let price = 500;
 let discount = 20;
