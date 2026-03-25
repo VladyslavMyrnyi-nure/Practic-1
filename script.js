6b59b6e811bb4d6a379b5d58b082218de5b9005d
Yehor-Zadorozhnyi <yehor.zadorozhnyi@nure.ua>
2026-03-25 17:25:19 +0200

    Додано основний код программи

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+// Алгоритм сортування бульбашкою
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+    }
